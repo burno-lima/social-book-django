46908358077a36ffbf3cfe3cd7afecfe3c04a604
Bruno Gustavo <bruno.lima@nappsolutions.com>
2022-09-27 11:17:23 -0300

    Account Settings

--- core/views.py
+++ core/views.py
@@ -5,9 +5,40 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Profile
 
+
 @login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
+
+
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':       
+
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
+        return redirect('settings')
+    return render(request, 'setting.html', {'user_profile': user_profile})
 
 
 def signup(request):
@@ -35,16 +66,20 @@ def signup(request):
 
                 # Log user in and redirect to settings page
 
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+
                 # create a Profile object for the new user
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
-                return redirect('signup')
+                return redirect('settings')
         else:
             messages.info(request, 'Password Not Matching')
             return redirect('signup')
 
     return render(request, 'signup.html')
+
 
 def signin(request):
     if request.method == 'POST':
